4d3b061494c0c6c6d73b12a3356d8a828aeab861
chakas11 <109025509+chakas11@users.noreply.github.com>
2025-05-02 18:19:21 -0400

    Add files via upload

---
+++ send_email.php
@@ -0,0 +1,36 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Get form data
+    $name = htmlspecialchars($_POST['name']);
+    $email = htmlspecialchars($_POST['email']);
+    $message = htmlspecialchars($_POST['message']);
+
+    // Set the recipient email address
+    $to = "patelvivekd@gmail.com";
+
+    // Set the email subject
+    $subject = "New Contact Form Submission from $name";
+
+    // Build the email content
+    $email_content = "Name: $name\n";
+    $email_content .= "Email: $email\n\n";
+    $email_content .= "Message:\n$message\n";
+
+    // Set the email headers
+    $headers = "From: $name <$email>\r\n";
+    $headers .= "Reply-To: $email\r\n";
+    $headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
+
+    // Send the email
+    if (mail($to, $subject, $email_content, $headers)) {
+        // Email sent successfully
+        echo "Thank you! Your message has been sent.";
+    } else {
+        // Failed to send email
+        echo "Oops! Something went wrong and we couldn't send your message.";
+    }
+} else {
+    // Not a POST request, redirect or handle accordingly
+    echo "Invalid request method.";
+}
+?>
